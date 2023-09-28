0e514eb44f93d91b3070828c70da4a3ce7a8c564
Manal <manal.ahmadi@uir.ac.ma>
2023-09-28 05:13:51 -0600

    modify nav code

--- Nike-Front/nike/src/componements/navbar/navbar.jsx
+++ Nike-Front/nike/src/componements/navbar/navbar.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import "./navbar.css"
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <a href="/">Logo</a>
-      </div>
-      <ul className="navbar-menu">
-        <li className="navbar-item">
-          <a href="/">Home</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/about">About</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/services">Services</a>
-        </li>
-        <li className="navbar-item">
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+import './navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <img src="nike-logo.png" alt="Nike Logo" />
+            </div>
+            <ul className="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">New and Features</a></li>
+                <li><a href="#">Men</a></li>
+                <li><a href="#">Women</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div className="icons">
+                <a href="#"><i className="fas fa-shopping-cart"></i></a>
+                <a href="#"><i className="fas fa-heart"></i></a>
+            </div>
+        </nav>
+    );
+};
 
 export default Navbar;
+
