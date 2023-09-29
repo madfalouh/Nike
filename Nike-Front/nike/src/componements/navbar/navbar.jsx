cc3b27de8e401a8028e0b36142898d3168f01fe9
Manal <manal.ahmadi@uir.ac.ma>
2023-09-28 18:03:49 -0600

    navbar modified

--- Nike-Front/nike/src/componements/navbar/navbar.jsx
+++ Nike-Front/nike/src/componements/navbar/navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './navbar.css'; // Import the CSS file for styling
-
+import nikeLogo from '../../assets/img/nikeLogo.png'
 const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="logo">
-                <img src="nike-logo.png" alt="Nike Logo" />
+                <img src={nikeLogo} style={{marginLeft : "50px"}}  alt="Nike Logo" width={80} />
             </div>
             <ul className="menu">
                 <li><a href="#">Home</a></li>
@@ -15,7 +15,7 @@ const Navbar = () => {
                 <li><a href="#">Contact</a></li>
             </ul>
             <div className="icons">
-                <a href="#"><i className="fas fa-shopping-cart"></i></a>
+                <a href="#"><i style={{marginRight :"35px"  }} className="fas fa-shopping-cart"></i></a>
                 <a href="#"><i className="fas fa-heart"></i></a>
             </div>
         </nav>
@@ -23,4 +23,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
