fcac960f92008724632f8f02d2c3af892ea9132e
Manal <manal.ahmadi@uir.ac.ma>
2023-10-13 20:55:42 -0600

    add navbar for dashboard

--- Nike-Front/nike/src/componements/dashboard/Navbardash.js
+++ Nike-Front/nike/src/componements/dashboard/Navbardash.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Navbardash.css'; // Create a new CSS file for Navbar
 import './Images/nikelogo.jpg'
-import './Icons/notifications.svg'
+import nikelogo from './Images/nikelogosvg.svg'
+import Notification from './Icons/notifications.svg'
 
 const Navbardash = () => {
   return (
     <div className="navbar">
-      <img src="nikelogo.jpg" alt="Logo" />
+      <img className='nikelogo' src={nikelogo}  alt="logo"/>
       <div className="search-bar">
         <input type="text" placeholder="Search" />
       </div>
       <div className="notification-square">
-        <img src="notifications.svg" alt="Notifications" />
+        <img src={Notification} alt="Notifications" />
       </div>
     </div>
   );
